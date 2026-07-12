e9ff55f2daafa64ac2767509b3d24b197f2d2b97
avinash <amenon848@gmail.com>
2026-07-12 11:14:36 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/avinashio/lazyspringboot/LazyspringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.avinashio.lazyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/avinashio/lazyspringboot/LazyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.avinashio.lazyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
